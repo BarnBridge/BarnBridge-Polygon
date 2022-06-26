f5de3d3656482b3a67520020fef49f0efe29311f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-26 11:31:36 +0800

    Delete contracts directory

--- contracts/interfaces/ISmartYieldController.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.5;
-pragma abicoder v2;
-
-interface ISmartYieldController {
-    function feesOwner() view external returns (address);
-    function setFeesOwner(address) external;
-}
-
