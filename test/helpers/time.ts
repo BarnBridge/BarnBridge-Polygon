b1759c5ae34b700a58eb078f72a04172e296ea05
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-26 11:34:36 +0800

    Delete test directory

--- test/helpers/time.ts
+++
@@ -1,8 +0,0 @@
-import { ethers } from 'hardhat';
-
-export const mineBlocks = async (blocks: number): Promise<void> => {
-  const blockBefore = await ethers.provider.getBlock('latest');
-  for (let f = 0; f < blocks; f++) {
-    await ethers.provider.send('evm_mine', []);
-  }
-};
