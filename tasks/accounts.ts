f97ab3f956adece528089f7462b55be138c03413
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-26 11:33:59 +0800

    Delete tasks directory

--- tasks/accounts.ts
+++
@@ -1,9 +0,0 @@
-import { HardhatUserConfig, task } from 'hardhat/config';
-
-task('accounts', 'Prints the list of accounts', async (args, hre) => {
-    const accounts = await hre.ethers.getSigners();
-
-    for (const account of accounts) {
-        console.log(await account.getAddress());
-    }
-});
